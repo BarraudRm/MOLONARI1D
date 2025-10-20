2d4ee53bb1c519c9b6592ac90fe3461d924fe4dd
Louise de Ferran <louise.deferran@orange.fr>
2025-10-20 12:17:17 +0200

    fusion dans la classe reader de readCSV

--- hardware/field_code/mains_ino/sensor/main.cpp
+++ hardware/field_code/mains_ino/sensor/main.cpp
@@ -10,19 +10,9 @@
 #include "LoRa_Molonari.hpp"
 #include "Time.hpp"
 #include "Waiter.hpp"
+#include "Reader.hpp"
 
- // ----- Structures -----
- struct SensorConfig {
-     String id;
-     String type; // "Pression ou Thermistance ou Thermocouple"
-     int pin;
-     float offset;
-     float scale;
- };
 
-// ----- Variables globales -----
- std::vector<SensorConfig> liste_capteurs; // Capteurs lus depuis CSV
-int FREQUENCE_MINUTES = 15; //initialisation par défaut
 int LORA_INTERVAL_H = 3;//initialisation par défaut
 
 Sensor** sens;
@@ -34,55 +24,9 @@ const char filename[] = "RECORDS.CSV";
 
 LoraCommunication lora(868E6, 0x01, 0x02, RoleType::SLAVE); // fréquence, adresse locale, adresse distante
 unsigned long lastLoRaSend = 0;
-unsigned long LORA_INTERVAL_S = 3UL * 3600UL; // initialisation par défaut
 unsigned long lastSDOffset = 0;
 std::queue<String> sendQueue;
 
-// ----- Lecture CSV -----
-void lireConfigCSV(const char* NomFichier) {
-    if (!SD.begin(CSPin)) { Serial.println("Impossible de monter SD"); return; }
-
-    File f = SD.open(NomFichier);
-    if (!f) { Serial.println("Fichier CSV non trouvé"); return; }
-
-    while (f.available()) {
-        String line = f.readStringUntil('\n');
-        //supprimer les espaces inutiles dans les lignes : 
-        line.trim(); 
-        if (line.length() == 0 || line.startsWith("#")) continue;
-
-        // Paramètres globaux
-        if (line.startsWith("mesure_frequence_minutes")) {
-            //trouver la place de la virgule, notre séparateur : 
-            int idx = line.indexOf(',');
-            FREQUENCE_MINUTES = line.substring(idx + 1).toInt();
-        } else if (line.startsWith("lora_intervalle_secondes")) {
-            int idx = line.indexOf(',');
-            LORA_INTERVAL_S = line.substring(idx + 1).toInt();
-        } else {
-            // Capteurs
-            String tokens[5];
-            int first = 0, last = 0, tokenIdx = 0;
-            while (last >= 0 && tokenIdx < 5) {
-                last = line.indexOf(',', first);
-                if (last < 0) last = line.length();
-                tokens[tokenIdx++] = line.substring(first, last);
-                first = last + 1;
-            }
-             SensorConfig c;
-             c.id = tokens[0];
-             c.type =tokens[1];
-             // Conversion des pins A0-A5 en int
-             //important de laisser A0 en fin de ligne pour la ledcture de Analograead
-             if (tokens[2].startsWith("A")) c.pin = tokens[2].substring(1).toInt() + A0;
-             else c.pin = tokens[2].toInt();
-             c.offset = tokens[3].toFloat();
-             c.scale = tokens[4].toFloat();
-             liste_capteurs.push_back(c);
-        }
-    }
-    f.close();
-}
 
 
 // ----- Setup -----
@@ -95,7 +39,8 @@ void setup() {
     while (!Serial && millis() < end_date) {}
 
     // Lecture de la configuration CSV
-    lireConfigCSV("config_sensor.csv");
+    Reader reader;
+    reader.lireConfigCSV("config_sensor.csv");
 
     // Compter les capteurs
     int ncapteur = 0; 
